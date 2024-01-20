6b783bab06c336b636f3d860617ad6fccc18b052
LukeMech <lukaszjajkommz@gmail.com>
2024-01-20 22:14:41 +0100

    yoo

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
+touch /tmp/INSTALLED_PACKAGES
+PACKAGES="python3"
+if [ ! "$PACKAGES" == "$(cat /tmp/INSTALLED_PACKAGES)" ]; then
+  cd /tmp
+  rm -rf notroot
+  git clone https://github.com/CrazyPython/notroot
+  source notroot/bashrc
+  notroot install $PACKAGES
+  echo $PACKAGES > /tmp/INSTALLED_PACKAGES
+else
+  source /tmp/notroot/bashrc
+fi
+
 python3 -m pip install -r requirements.txt
 
 python3 app.py
