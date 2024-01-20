3926674912547043a9dc7d9bb5056f4175c576b7
LukeMech <lukaszjajkommz@gmail.com>
2024-01-20 22:05:20 +0100

    XD

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Home page
 @app.route('/')
-def hello():
-    return 'Hello, Glitch! This is a Python web server.'
+def home():
+    return 'Welcome to my Python website!'
 
 if __name__ == '__main__':
     app.run()
